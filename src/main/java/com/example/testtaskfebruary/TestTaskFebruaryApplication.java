a33ef2243f839785e54e696b9e9da5b24d43cfc3
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2025-02-12 12:01:26 +0300

    Init project

---
+++ src/main/java/com/example/testtaskfebruary/TestTaskFebruaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtaskfebruary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskFebruaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskFebruaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testtaskfebruary/TestTaskFebruaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtaskfebruary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskFebruaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
